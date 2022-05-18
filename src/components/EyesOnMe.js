7fa48e4e815af9bac8e8c3e20c9acf2ea0499821
Manuel Baez Diaz <baezdiazm@yahoo.com>
2022-05-17 23:39:56 -0400

    commit tree ff13b69b1eb0042f8b3b63d4055be50277a2f81d

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+ import React from "react"
+
+ function handleFocus(e) {
+    console.log("Good!")
+ }
+
+ function handleBlurr(e) {
+    console.log("Hey! Eyes on me!")
+ }
+
+ function EyesOnME() {
+     return (
+         <>
+          <button onFocus={handleFocus} onBlur={handleBlurr}>Eyes on me</button>
+         </>
+     )
+ }
+
+ export default EyesOnME;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from "react"
+
+function handlePW(e) {
+    console.log("Entering password...")
+}
+
+function Keypad() {
+    return(
+        <>
+            <input type="password" onChange={handlePW}/>
+
+        </>
+    )
+}
+
+export default Keypad;
